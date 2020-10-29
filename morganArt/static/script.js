8fda26e71f8cdec874c8cfe36fde033c6b5392ee
Anthony Parrino <anthony21parrino21@gmail.com>
2020-10-29 18:31:58 -0400

    added new html files for nav bar

--- morganArt/static/script.js
+++ morganArt/static/script.js
@@ -4,8 +4,8 @@ console.log("home");
 var featuredPainting = $("#featured-painting-image")
 function featuredClick() {
     console.log("clicked");
+    
 }
-
 
     featuredPainting.on("click", featuredClick);
 });
